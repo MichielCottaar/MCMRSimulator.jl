d156b7ba63e22f7613b43a4645b823c061a60414
Michiel Cottaar <MichielCottaar@protonmail.com>
2022-07-14 18:36:14 +0100

    Added RF pulses

--- src/MRSimulator.jl
+++ src/MRSimulator.jl
@@ -13,6 +13,8 @@ mutable struct Spin
     Spin(;time=0., position=zero(SVector{3,Real}), longitudinal=1., transverse=0., phase=0.) = new(time, position, longitudinal, transverse, phase)
 end
 
+# Defining the microstructure seen by the spin
+
 abstract type Field{T} end
 
 struct ZeroField{T} <: Field{T}
@@ -44,10 +46,57 @@ struct Microstructure
     Microstructure(;off_resonance=ZeroField{Float64}()) = new(off_resonance)
 end
 
-
 (micro::Microstructure)(position :: SVector{3,Real}) = LocalEnvironment(
     micro.off_resonance(position)
 )
+
+# defining the sequence
+abstract type SequenceComponent end
+
+struct RFPulse <: SequenceComponent
+    time :: Real
+    flip_angle :: Real
+    cf :: Real
+    sf :: Real
+    phase :: Real
+    cp :: Real
+    sp :: Real
+    RFPulse(time, flip_angle, phase) = begin
+        f = flip_angle / 180. * π
+        p = phase / 180. * π
+        new(time, f, cos(f), sin(f), p, cos(p), sin(p))
+    end
+end
+
+function apply!(pulse :: RFPulse, spin :: Spin)
+    @assert pulse.time == spin.time
+    Bx_init = spin.transverse * cos(spin.phase)
+    By_init = spin.transverse * sin(spin.phase)
+    Bxy_parallel  = pulse.cp * Bx_init + pulse.sp * By_init
+    Bxy_perp_init = pulse.cp * By_init - pulse.sp * Bx_init
+
+    Bxy_perp = Bxy_perp_init * pulse.cf + spin.longitudinal * pulse.sf
+    spin.longitudinal = spin.longitudinal * pulse.cf - Bxy_perp_init * pulse.sf
+    Bx = Bxy_parallel * pulse.cp - Bxy_perp * pulse.sp
+    By = Bxy_perp * pulse.cp + Bxy_parallel * pulse.sp
+    spin.transverse = sqrt(Bx * Bx + By * By)
+    spin.phase = atan(By, Bx)
+end
+
+struct Sequence
+    pulses :: Vector{RFPulse}
+    function Sequence(pulses::Vector{RFPulse})
+        for c in components
+            append!(result, (c.time, c))
+        end
+        new(sort(pulses, by=x->x.time))
+    end
+end
+
+function next_time(current_time :: Real, sequence :: Sequence)
+    index = searchsortedfirst(sequence.components, current_time, by=x->x.time)
+    return sequence[index].time
+end
 
 function relax!(spin :: Spin, micro :: Microstructure, timestep :: Real, B0=3.)
     @assert timestep > 0
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,9 @@
 using Test
-import MRSimulator: Spin, ZeroField, Microstructure, evolve!, time, ConstantField, GradientField, gyromagnetic_ratio
+import MRSimulator: Spin, ZeroField, Microstructure, evolve!, time, ConstantField, GradientField, gyromagnetic_ratio, RFPulse, apply!
 using StaticArrays
 
 @testset "MRSimulator.jl" begin
-    @testset "Testing one feature at a time" begin
+    @testset "Generate and apply microstructural fields" begin
         @testset "Simulate empty environment/sequence" begin
             spin = evolve!(Spin(), Microstructure(), 1.)
             @test spin.time == 1.
@@ -29,6 +29,94 @@ using StaticArrays
             @test spin.time == 0.5
             @test spin.position == SA_F64[3., 0., 0.]
             @test spin.phase ≈ (0.6 + (2. + 1.5 * 3.) * 0.2) * 3 * gyromagnetic_ratio
+        end
+    end
+    @testset "Apply RF pulses" begin
+        @testset "0 degree pulses should do nothing" begin
+            for pulse_phase in (-90, -45, 0., 30., 90., 180, 22.123789)
+                pulse = RFPulse(0., 0., pulse_phase)
+                for spin_phase in (-90, -45, 0., 30., 90., 180, 22.123789)
+                    spin = Spin(phase=spin_phase / 180 * π, transverse=1.)
+                    apply!(pulse, spin)
+                    @test spin.phase ≈ (spin_phase % 360) / 180 * π
+                    @test spin.longitudinal ≈ 1.
+                    @test spin.transverse ≈ 1.
+                end
+            end
+        end
+        @testset "180 degree pulses should flip longitudinal" begin
+            for pulse_phase in (-90, -45, 0., 30., 90., 180, 22.123789)
+                pulse = RFPulse(0., 180., pulse_phase)
+                spin = Spin()
+                @test spin.longitudinal == 1.
+                apply!(pulse, spin)
+                @test spin.longitudinal ≈ -1.
+            end
+        end
+        @testset "90 degree pulses should eliminate longitudinal" begin
+            for pulse_phase in (-90, -45, 0., 30., 90., 180, 22.123789)
+                pulse = RFPulse(0., 90., pulse_phase)
+                spin = Spin()
+                @test spin.longitudinal == 1.
+                apply!(pulse, spin)
+                @test spin.longitudinal ≈ 0. atol=1e-12
+            end
+        end
+        @testset "Spins with same phase as pulse are unaffected by pulse" begin
+            for phase in (-90, -45, 0., 30., 90., 180, 22.123789)
+                for flip_angle in (10, 90, 120, 180)
+                    pulse = RFPulse(0., flip_angle, phase)
+
+                    spin_phase = phase / 180 * π
+                    spin = Spin(longitudinal=0., transverse=1., phase=spin_phase)
+                    apply!(pulse, spin)
+                    @test spin.longitudinal ≈ 0. atol=1e-12
+                    @test spin.transverse ≈ 1.
+                    @test spin.phase ≈ spin_phase
+                end
+            end
+        end
+        @testset "180 pulses flips phase around axis" begin
+            for phase in (0., 22., 30., 80.)
+                pulse = RFPulse(0., 180, 0.)
+
+                spin_phase = phase / 180 * π
+                spin = Spin(longitudinal=0., transverse=1., phase=spin_phase)
+                apply!(pulse, spin)
+                @test spin.longitudinal ≈ 0. atol=1e-12
+                @test spin.transverse ≈ 1.
+                @test spin.phase ≈ -spin_phase
+            end
+            for phase in (0., 22., 30., 80.)
+                pulse = RFPulse(0., 180, phase)
+
+                spin_phase = phase / 180 * π
+                spin = Spin(longitudinal=0., transverse=1., phase=0.)
+                apply!(pulse, spin)
+                @test spin.longitudinal ≈ 0. atol=1e-12
+                @test spin.transverse ≈ 1.
+                @test spin.phase ≈ 2 * phase / 180 * π
+            end
+        end
+        @testset "90 pulses flips longitudinal spin into transverse plane" begin
+            for phase in (0., 22., 30., 80.)
+                pulse = RFPulse(0., 90, phase)
+                spin = Spin()
+                apply!(pulse, spin)
+                @test spin.longitudinal ≈ 0. atol=1e-12
+                @test spin.transverse ≈ 1.
+                @test spin.phase ≈ (phase + 90) / 180 * π
+            end
+        end
+        @testset "90 pulses flips transverse spin into longitudinal plane" begin
+            for phase in (0., 22., 30., 80.)
+                pulse = RFPulse(0., 90, phase)
+                spin_phase = (phase + 90) / 180 * π
+                spin = Spin(longitudinal=0, transverse=1., phase=spin_phase)
+                apply!(pulse, spin)
+                @test spin.longitudinal ≈ -1.
+                @test spin.transverse ≈ 0. atol=1e-12
+            end
         end
     end
 end
