cd48e28731cba1b5908f1f67c33e712e10870e46
Michiel Cottaar <MichielCottaar@protonmail.com>
2022-07-14 16:33:44 +0100

    Initial simple off-resonance fields

--- src/MRSimulator.jl
+++ src/MRSimulator.jl
@@ -1,5 +1,59 @@
 module MRSimulator
+using StaticArrays
 
-# Write your package code here.
+mutable struct Spin
+    time :: Real
+    position :: SVector{3,Real}
+    phase :: Real
+    Spin(;time=0., position=zero(SVector{3,Real}), phase=0.) = new(time, position, phase)
+end
+
+abstract type Field{T} end
+
+struct ZeroField{T} <: Field{T}
+end
+
+(f::ZeroField{T})(position :: SVector{3,Real}) where {T} = zero(T)
+
+
+struct ConstantField{T} <: Field{T}
+    value :: T
+end
+
+(f::ConstantField{T})(position :: SVector{3,Real}) where {T} = f.value
+
+
+struct LocalEnvironment
+    off_resonance :: Real
+end
+
+
+struct Microstructure
+    off_resonance :: Field
+    Microstructure(;off_resonance=ZeroField{Float64}()) = new(off_resonance)
+end
+
+
+(micro::Microstructure)(position :: SVector{3,Real}) = LocalEnvironment(
+    micro.off_resonance(position)
+)
+
+function relax!(spin :: Spin, micro :: Microstructure, timestep :: Real)
+    @assert timestep > 0
+    environment = micro(spin.position)
+    spin.phase += environment.off_resonance * timestep
+end
+
+function evolve!(spin :: Spin, micro :: Microstructure, new_time :: Real)
+    if spin.time > new_time
+        throw(DomainError("Spins cannot travel backwards in time"))
+    end
+    if new_time > spin.time
+        timestep = new_time - spin.time
+        relax!(spin, micro, timestep)
+        spin.time = new_time
+    end
+    spin
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,19 @@
-using MRSimulator
 using Test
+import MRSimulator: Spin, ZeroField, Microstructure, evolve!, time, ConstantField
+using StaticArrays
 
 @testset "MRSimulator.jl" begin
     # Write your tests here.
+    @testset "Simulate empty environment/sequence" begin
+        spin = evolve!(Spin(), Microstructure(), 1.)
+        @test spin.time == 1.
+        @test spin.position == SA_F64[0., 0., 0.]
+        @test spin.phase == 0.
+    end
+    @testset "Test constant off-resonance field" begin
+        spin = evolve!(Spin(), Microstructure(off_resonance=ConstantField(2.)), 0.3)
+        @test spin.time == 0.3
+        @test spin.position == SA_F64[0., 0., 0.]
+        @test spin.phase == 0.6
+    end
 end
