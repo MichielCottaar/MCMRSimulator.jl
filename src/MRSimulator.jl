140c6d1f929cb46b4ac02b70c820422625aa7342
Michiel Cottaar <MichielCottaar@protonmail.com>
2022-07-18 11:56:49 +0100

    Made spins immutable

--- src/MRSimulator.jl
+++ src/MRSimulator.jl
@@ -2,15 +2,24 @@ module MRSimulator
 using StaticArrays
 using LinearAlgebra
 
-const gyromagnetic_ratio = 267.52218744  # (10^6 rad⋅s^−1⋅T^−1)
+const gyromagnetic_ratio = 1 # 267.52218744  # (10^6 rad⋅s^−1⋅T^−1)
 
-mutable struct Spin
-    time :: Real
-    position :: SVector{3,Real}
+struct SpinOrientation
     longitudinal :: Real
     transverse :: Real
     phase :: Real
-    Spin(;time=0., position=zero(SVector{3,Real}), longitudinal=1., transverse=0., phase=0.) = new(time, position, longitudinal, transverse, phase)
+end
+
+struct Spin
+    time :: Real
+    position :: SVector{3,Real}
+    orientation :: SpinOrientation
+end
+Spin(;time=0., position=zero(SVector{3,Real}), longitudinal=1., transverse=0., phase=0.) = Spin(time, position, SpinOrientation(longitudinal, transverse, phase))
+
+for param in (:longitudinal, :transverse, :phase)
+    @eval $param(o :: SpinOrientation) = o.$param
+    @eval $param(s :: Spin) = $param(s.orientation)
 end
 
 # Defining the microstructure seen by the spin
@@ -68,19 +77,20 @@ struct RFPulse <: SequenceComponent
     end
 end
 
-function apply!(pulse :: RFPulse, spin :: Spin)
-    @assert pulse.time == spin.time
+function apply_pulse(pulse :: RFPulse, spin :: SpinOrientation)
     Bx_init = spin.transverse * cos(spin.phase)
     By_init = spin.transverse * sin(spin.phase)
     Bxy_parallel  = pulse.cp * Bx_init + pulse.sp * By_init
     Bxy_perp_init = pulse.cp * By_init - pulse.sp * Bx_init
 
     Bxy_perp = Bxy_perp_init * pulse.cf + spin.longitudinal * pulse.sf
-    spin.longitudinal = spin.longitudinal * pulse.cf - Bxy_perp_init * pulse.sf
     Bx = Bxy_parallel * pulse.cp - Bxy_perp * pulse.sp
     By = Bxy_perp * pulse.cp + Bxy_parallel * pulse.sp
-    spin.transverse = sqrt(Bx * Bx + By * By)
-    spin.phase = atan(By, Bx)
+    SpinOrientation(
+        spin.longitudinal * pulse.cf - Bxy_perp_init * pulse.sf,
+        sqrt(Bx * Bx + By * By),
+        atan(By, Bx)
+    )
 end
 
 struct Sequence
@@ -98,20 +108,27 @@ function next_time(current_time :: Real, sequence :: Sequence)
     return sequence[index].time
 end
 
-function relax!(spin :: Spin, micro :: Microstructure, timestep :: Real, B0=3.)
+function relax(orient :: SpinOrientation, env :: LocalEnvironment, timestep :: Real, B0=3.)
     @assert timestep > 0
-    environment = micro(spin.position)
-    spin.phase += environment.off_resonance * timestep * gyromagnetic_ratio * B0
+    SpinOrientation(
+        orient.longitudinal,
+        orient.transverse,
+        env.off_resonance * timestep * gyromagnetic_ratio * B0 + orient.phase
+    )
+
 end
 
-function evolve!(spin :: Spin, micro :: Microstructure, new_time :: Real, B0=3.)
+function evolve_to_time(spin :: Spin, micro :: Microstructure, new_time :: Real, B0=3.)
     if spin.time > new_time
         throw(DomainError("Spins cannot travel backwards in time"))
     end
     if new_time > spin.time
         timestep = new_time - spin.time
-        relax!(spin, micro, timestep, B0)
-        spin.time = new_time
+        spin = Spin(
+            new_time,
+            spin.position,
+            relax(spin.orientation, micro(spin.position), timestep, B0)
+        )
     end
     spin
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,34 +1,32 @@
 using Test
-import MRSimulator: Spin, ZeroField, Microstructure, evolve!, time, ConstantField, GradientField, gyromagnetic_ratio, RFPulse, apply!
+import MRSimulator: Spin, ZeroField, Microstructure, evolve_to_time, time, ConstantField, GradientField, gyromagnetic_ratio, RFPulse, apply_pulse, phase, longitudinal, transverse
 using StaticArrays
 
 @testset "MRSimulator.jl" begin
     @testset "Generate and apply microstructural fields" begin
         @testset "Simulate empty environment/sequence" begin
-            spin = evolve!(Spin(), Microstructure(), 1.)
+            spin = evolve_to_time(Spin(), Microstructure(), 1.)
             @test spin.time == 1.
             @test spin.position == SA_F64[0., 0., 0.]
-            @test spin.phase == 0.
+            @test phase(spin) == 0.
         end
         @testset "Test constant off-resonance field" begin
-            spin = evolve!(Spin(), Microstructure(off_resonance=ConstantField(2.)), 0.3, 3.)
+            spin = evolve_to_time(Spin(), Microstructure(off_resonance=ConstantField(2.)), 0.3, 3.)
             @test spin.time == 0.3
             @test spin.position == SA_F64[0., 0., 0.]
-            @test spin.phase ≈ 0.6 * 3 * gyromagnetic_ratio
+            @test spin.orientation.phase ≈ 0.6 * 3 * gyromagnetic_ratio
         end
         @testset "Test gradient off-resonance field" begin
             micro = Microstructure(off_resonance=GradientField(SA_F64[1.5, 0., 0.], 2.))
-            spin = Spin()
-            evolve!(spin, micro, 0.3, 3.)
+            spin = evolve_to_time(Spin(), micro, 0.3, 3.)
             @test spin.time == 0.3
             @test spin.position == SA_F64[0., 0., 0.]
-            @test spin.phase ≈ 0.6 * 3 * gyromagnetic_ratio
+            @test spin.orientation.phase ≈ 0.6 * 3 * gyromagnetic_ratio
             # Move spin and evolve a bit further in time
-            spin.position = SA_F64[3., 0., 0.]
-            evolve!(spin, micro, 0.5, 3.)
+            spin = evolve_to_time(Spin(0.3, SA_F64[3., 0., 0.], spin.orientation), micro, 0.5, 3.)
             @test spin.time == 0.5
             @test spin.position == SA_F64[3., 0., 0.]
-            @test spin.phase ≈ (0.6 + (2. + 1.5 * 3.) * 0.2) * 3 * gyromagnetic_ratio
+            @test spin.orientation.phase ≈ (0.6 + (2. + 1.5 * 3.) * 0.2) * 3 * gyromagnetic_ratio
         end
     end
     @testset "Apply RF pulses" begin
@@ -37,7 +35,7 @@ using StaticArrays
                 pulse = RFPulse(0., 0., pulse_phase)
                 for spin_phase in (-90, -45, 0., 30., 90., 180, 22.123789)
                     spin = Spin(phase=spin_phase / 180 * π, transverse=1.)
-                    apply!(pulse, spin)
+                    spin = apply_pulse(pulse, spin.orientation)
                     @test spin.phase ≈ (spin_phase % 360) / 180 * π
                     @test spin.longitudinal ≈ 1.
                     @test spin.transverse ≈ 1.
@@ -48,8 +46,8 @@ using StaticArrays
             for pulse_phase in (-90, -45, 0., 30., 90., 180, 22.123789)
                 pulse = RFPulse(0., 180., pulse_phase)
                 spin = Spin()
-                @test spin.longitudinal == 1.
-                apply!(pulse, spin)
+                @test spin.orientation.longitudinal == 1.
+                spin = apply_pulse(pulse, spin.orientation)
                 @test spin.longitudinal ≈ -1.
             end
         end
@@ -57,8 +55,8 @@ using StaticArrays
             for pulse_phase in (-90, -45, 0., 30., 90., 180, 22.123789)
                 pulse = RFPulse(0., 90., pulse_phase)
                 spin = Spin()
-                @test spin.longitudinal == 1.
-                apply!(pulse, spin)
+                @test spin.orientation.longitudinal == 1.
+                spin = apply_pulse(pulse, spin.orientation)
                 @test spin.longitudinal ≈ 0. atol=1e-12
             end
         end
@@ -69,7 +67,7 @@ using StaticArrays
 
                     spin_phase = phase / 180 * π
                     spin = Spin(longitudinal=0., transverse=1., phase=spin_phase)
-                    apply!(pulse, spin)
+                    spin = apply_pulse(pulse, spin.orientation)
                     @test spin.longitudinal ≈ 0. atol=1e-12
                     @test spin.transverse ≈ 1.
                     @test spin.phase ≈ spin_phase
@@ -82,7 +80,7 @@ using StaticArrays
 
                 spin_phase = phase / 180 * π
                 spin = Spin(longitudinal=0., transverse=1., phase=spin_phase)
-                apply!(pulse, spin)
+                spin = apply_pulse(pulse, spin.orientation)
                 @test spin.longitudinal ≈ 0. atol=1e-12
                 @test spin.transverse ≈ 1.
                 @test spin.phase ≈ -spin_phase
@@ -92,7 +90,7 @@ using StaticArrays
 
                 spin_phase = phase / 180 * π
                 spin = Spin(longitudinal=0., transverse=1., phase=0.)
-                apply!(pulse, spin)
+                spin = apply_pulse(pulse, spin.orientation)
                 @test spin.longitudinal ≈ 0. atol=1e-12
                 @test spin.transverse ≈ 1.
                 @test spin.phase ≈ 2 * phase / 180 * π
@@ -102,7 +100,7 @@ using StaticArrays
             for phase in (0., 22., 30., 80.)
                 pulse = RFPulse(0., 90, phase)
                 spin = Spin()
-                apply!(pulse, spin)
+                spin = apply_pulse(pulse, spin.orientation)
                 @test spin.longitudinal ≈ 0. atol=1e-12
                 @test spin.transverse ≈ 1.
                 @test spin.phase ≈ (phase + 90) / 180 * π
@@ -113,7 +111,7 @@ using StaticArrays
                 pulse = RFPulse(0., 90, phase)
                 spin_phase = (phase + 90) / 180 * π
                 spin = Spin(longitudinal=0, transverse=1., phase=spin_phase)
-                apply!(pulse, spin)
+                spin = apply_pulse(pulse, spin.orientation)
                 @test spin.longitudinal ≈ -1.
                 @test spin.transverse ≈ 0. atol=1e-12
             end
