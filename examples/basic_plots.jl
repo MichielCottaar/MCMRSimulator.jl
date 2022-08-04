aea6dc7cbf431a5bb4ceaa500ba2ca7931138876
Michiel Cottaar <MichielCottaar@protonmail.com>
2022-08-04 15:44:04 +0100

    Create sequence plot using Makie

---
+++ examples/basic_plots.jl
@@ -0,0 +1,40 @@
+### A Pluto.jl notebook ###
+# v0.19.9
+
+using Markdown
+using InteractiveUtils
+
+# This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
+macro bind(def, element)
+    quote
+        local iv = try Base.loaded_modules[Base.PkgId(Base.UUID("6e696c72-6542-2067-7265-42206c756150"), "AbstractPlutoDingetjes")].Bonds.initial_value catch; b -> missing; end
+        local el = $(esc(element))
+        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : iv(el)
+        el
+    end
+end
+
+# ╔═╡ d990be32-1343-11ed-0acb-0fdad105c87c
+begin
+	import Pkg
+	Pkg.activate()
+	using JSServe
+	using WGLMakie
+	using MRSimulator
+	using PlutoUI
+    Page()
+end
+
+# ╔═╡ ecac5fd3-313c-407d-a299-6ec1825dd9c8
+@bind TE PlutoUI.Slider(5:50, default=20, show_value=true)
+
+# ╔═╡ 9512c409-850c-4b7d-ac5a-ecbacf706f9d
+begin
+	sequence = Sequence([RFPulse(flip_angle=90), RFPulse(flip_angle=180, time=TE/2.)], 1.5 * TE)
+	MRSimulator.sequenceplot(sequence)
+end
+
+# ╔═╡ Cell order:
+# ╠═d990be32-1343-11ed-0acb-0fdad105c87c
+# ╠═ecac5fd3-313c-407d-a299-6ec1825dd9c8
+# ╠═9512c409-850c-4b7d-ac5a-ecbacf706f9d
--- src/plot.jl
+++ src/plot.jl
@@ -1,1 +1,27 @@
+import Makie: Makie, @lift
+
 color(orient::Union{Spin, SpinOrientation}; saturation=1., value=1.) = Colors.HSVA(phase(orient) + 180, saturation, value, transverse(orient))
+
+@Makie.recipe(SequencePlot, seq) do scene
+    Makie.Theme(
+    )
+end
+
+function Makie.plot!(sp::SequencePlot)
+    seq = sp[1]
+    times = @lift [p.time for p in $seq.pulses]
+    flip_angle = @lift [rad2deg(p.flip_angle) for p in $seq.pulses]
+    as_zero = @lift $flip_angle * 0
+    max_angle = @lift maximum($flip_angle)
+
+    yval = @lift $flip_angle ./ $max_angle
+    Makie.arrows!(sp, times, as_zero, as_zero, yval)
+
+    text = @lift [string(Int(round(a))) for a in $flip_angle]
+    text_positions = @lift [(t, 0.05 + y) for (t, y) in zip($times, $yval)]
+    Makie.text!(sp, text, position=text_positions, align=(:center, :center))
+    #vlines!(sp, TR, color="black")
+    sp
+end
+
+Makie.plottype(::Sequence) = SequencePlot
