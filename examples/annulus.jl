ae72897383632d9a15424b67703dd6291c977688
Michiel Cottaar <MichielCottaar@protonmail.com>
2022-08-07 10:31:48 +0100

    Test free diffusion

--- examples/annulus.jl
+++ examples/annulus.jl
@@ -1,20 +1,20 @@
 import MRSimulator as mr
+using StaticArrays
 # Set up infinitely repeating aligned cylinders
 outer = mr.Cylinder(0.9)
 inner = mr.Cylinder(0.8)
 geometry = mr.Repeated([inner, outer], [2., 2., Inf])
 
 # Build spin echo diffusion-weighted sequence with perfect pulses and instantaneous gradients
-TR = 2000.
+TR = 200.
 TE = 80.
-qval = 0.
-sequence = mr.perfect_dwi(TR=TR, TE=TE, qval=qval)
+bval = 2.
+sequence = mr.perfect_dwi(TR=TR, TE=TE, bval=bval)
 
 micro = mr.Microstructure(diffusivity=mr.field(3.))
 
-spins = [mr.Spin(position=rand(mr.PosVector)) for _ in 1:30]
-
-@profview mr.evolve_TR(spins, sequence, micro)
+spins = [mr.Spin(position=SA_F64[0., 0., z]) for z in 1:30000];
 
 
-res = mr.evolve_TR(spins, sequence, micro)
+res = mr.evolve_TR(spins, sequence, micro);
+@time mr.evolve_TR(spins, sequence, micro);
