22e83edd6123947972eec745dd1e4e6ae9960b48
Michiel Cottaar <MichielCottaar@protonmail.com>
2022-08-08 15:56:29 +0100

    Write module-level documentation

--- src/MRSimulator.jl
+++ src/MRSimulator.jl
@@ -1,3 +1,13 @@
+"""
+This package supports the running of MR Monte Carlo simulations.
+
+In these simulations hundreds of thousands or millions of particles randomly diffuse through some tissue microstructure
+constrained by a series of [`Obstruction`](@ref) objects.
+The spins of these particles will be evolved by the effect of one or more [`Sequence`](@ref) objects as well as
+spatially varying [`Field`](@ref) objecs describing the R1, R2, diffusivity, and off-resonance fields.
+All of these variables are combined into a single [`Simulation`](@ref) object, which can then be evolved using [`append!`](@ref)(simulation).
+Plotting support for the output is also available based on [Makie.jl](https://makie.juliaplots.org/stable/).
+"""
 module MRSimulator
 import StaticArrays: SA_F64, MVector, SVector, @SVector, MMatrix
 using LinearAlgebra
