91dcb4d3978f753d58b1a4bdb567d27352674c67
Michiel Cottaar <MichielCottaar@protonmail.com>
2022-08-10 13:42:33 +0100

    Moved microstructure to its own file

--- src/constants.jl
+++ src/constants.jl
@@ -8,14 +8,6 @@ const SA = SA_F64
 "General definition used for length-3 vectors. Mostly used for positions."
 const PosVector = SVector{3, Float}
 
-"""
-Supertype of any obstruction to the free diffusion of water (e.g., [`Wall`](@ref), mesh, [`Cylinder`](@ref), [`Sphere`](@ref)).
-"""
-abstract type Obstruction end
-
-"Sequence of [`Obstruction`](@ref) objects that the particles have to navigate."
-const Obstructions{N, T} = SVector{N, T} where {T <: Obstruction}
-
 "Intermediate object used internally to represent a movement from one position to another"
 struct Movement
     origin :: PosVector
---
+++ src/microstructure.jl
@@ -0,0 +1,64 @@
+"The off-resonance, R2, and R1 values at a single point in space"
+struct LocalEnvironment
+    off_resonance :: Float
+    R2 :: Float
+    R1 :: Float
+end
+
+"""
+    Microstructure(; off_resonance=0., R2=0., R1=0., diffusivity=0., geometry=Obstruction[])
+
+Describes the microstructure of the tissue.
+
+This describes both the spatial distribution of the `R1`, `R2`, `off-resonance`, and `diffusivity` parameters (as [`Field`](@ref) objects)
+as well as the spatial `geometry` constraining the diffusion (as a sequence of [`Obstruction`](@ref) objects).
+
+The `R1`, `R2`, `off-resonance`, and `diffusivity` parameters can be defined as one of:
+- `value::Number`: constant value across the microstructure.
+- `(gradient::PosVector, value::Number)`: gradient across the microstructure.
+- `field::Field`: as generated using [`field`](@ref).
+"""
+struct Microstructure{F1 <: Field, F2 <: Field, F3 <: Field, F4 <: Field, G <: Obstructions}
+    off_resonance :: F1  # in ppm
+    R2 :: F2
+    R1 :: F3
+    diffusivity :: F4
+    geometry :: G
+    function Microstructure(;off_resonance=0., R2=0., R1=0., diffusivity=0., geometry=Obstruction[]) 
+        if isa(geometry, Obstruction)
+            geometry = [geometry]
+        end
+        R1 = field(R1)
+        R2 = field(R2)
+        diffusivity = field(diffusivity)
+        off_resonance = field(off_resonance)
+        geometry = SVector{length(geometry)}(geometry)
+        new{typeof(off_resonance), typeof(R2), typeof(R1), typeof(diffusivity), typeof(geometry)}(
+            off_resonance, R2, R1, diffusivity, geometry)
+    end
+end
+
+(micro::Microstructure)(position) = LocalEnvironment(
+    micro.off_resonance(position),
+    micro.R2(position),
+    micro.R1(position),
+)
+
+"""
+    relax(orientation::SpinOrientation, environment::LocalEnvironment, timestep::Real, B0=3.)
+
+Relaxes the spin `orientation` within the R1, R2, and off-resonance given by the local `environment` over time `timestep`
+"""
+function relax(orientation :: SpinOrientation, environment :: LocalEnvironment, timestep :: Real, B0=3.)
+    @assert timestep >= 0
+    if iszero(timestep)
+        return orientation
+    end
+    timestep = Float(timestep)
+    B0 = Float(B0)
+    SpinOrientation(
+        (1 - (1 - orientation.longitudinal) * exp(-environment.R1 * timestep)),
+        orientation.transverse * exp(-environment.R2 * timestep),
+        environment.off_resonance * timestep * gyromagnetic_ratio * B0 + orientation.phase
+    )
+end
