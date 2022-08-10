ede105b02c2fca0cb0fcae4025e0e4a527eb1ffd
Michiel Cottaar <MichielCottaar@protonmail.com>
2022-08-10 13:42:44 +0100

    move generic functions to geometry.jl

--- src/geometry/diffuse.jl
+++ src/geometry/diffuse.jl
@@ -126,24 +126,3 @@ function detect_collision(movement :: Movement, obstructions :: Obstructions, or
     collision
 end
 
-
-"""
-    isinside(position, obstructions)
-    isinside(spin, obstructions)
-    isinside(snapshot, obstructions)
-
-Test whether the particles are inside any of the obstructions.
-"""
-isinside(pos::Vector, o) = isinside(PosVector(pos), o)
-isinside(spin::Union{Spin, MultiSpin}, o) = isinside(position(spin), o)
-isinside(snapshot::Union{Snapshot, MultiSnapshot}, o) = map(s -> isinside(s, o), snapshot.spins)
-isinside(pos::PosVector, obstructions::Obstructions) = any(o -> isinside(pos, o), obstructions)
-
-
-"""
-    project(position, repeat::Repeated)
-    project(position, transform::Transformed)
-
-Computes the position in the space of the obstructions wrapped by the [`Repeated`](@ref) or [`Transformed`](@ref).
-"""
-function project end
--- src/geometry/geometry.jl
+++ src/geometry/geometry.jl
@@ -1,3 +1,33 @@
+"""
+Supertype of any obstruction to the free diffusion of water (e.g., [`Wall`](@ref), mesh, [`Cylinder`](@ref), [`Sphere`](@ref)).
+"""
+abstract type Obstruction end
+
+"Sequence of [`Obstruction`](@ref) objects that the particles have to navigate."
+const Obstructions{N, T} = SVector{N, T} where {T <: Obstruction}
+
+"""
+    isinside(position, obstructions)
+    isinside(spin, obstructions)
+    isinside(snapshot, obstructions)
+
+Test whether the particles are inside any of the obstructions.
+"""
+isinside(pos::Vector, o) = isinside(PosVector(pos), o)
+isinside(spin::Union{Spin, MultiSpin}, o) = isinside(position(spin), o)
+isinside(snapshot::Union{Snapshot, MultiSnapshot}, o) = map(s -> isinside(s, o), snapshot.spins)
+isinside(pos::PosVector, obstructions::Obstructions) = any(o -> isinside(pos, o), obstructions)
+
+
+"""
+    project(position, repeat::Repeated)
+    project(position, transform::Transformed)
+
+Computes the position in the space of the obstructions wrapped by the [`Repeated`](@ref) or [`Transformed`](@ref).
+"""
+function project end
+
+
 include("diffuse.jl")
 include("ray_grid_intersections.jl")
 include("repeat.jl")
