1455d08e6a27ab6da45f7d5188d8f33ea553c3ef
Michiel Cottaar <MichielCottaar@protonmail.com>
2022-08-12 16:12:57 +0100

    run for box mesh

--- examples/annulus.jl
+++ examples/annulus.jl
@@ -5,17 +5,17 @@ outer = mr.Cylinder(0.9)
 inner = mr.Cylinder(0.8)
 geometry = mr.Repeated([inner, outer], [2., 2., Inf])
 
+mb = mr.Repeated(mr.box_mesh(), [1.5, 1.5, 1.5])
+
 # Build spin echo diffusion-weighted sequence with perfect pulses and instantaneous gradients
 TR = 200.
 TE = 80.
 bval = 2.
 sequence = mr.perfect_dwi(TR=TR, TE=TE, bval=bval)
 
-micro = mr.Microstructure(diffusivity=mr.field(3.), geometry=geometry)
-
 snap = mr.Snapshot(3000);
 
-simulation = mr.Simulation(snap, [sequence], micro);
+simulation = mr.Simulation(snap, [sequence], diffusivity=3., geometry=[mb]);
 
 append!(simulation, 2.);
 @time append!(simulation, 200.);
