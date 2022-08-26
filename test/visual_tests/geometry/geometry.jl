c54b182b9388c6cb133c4c9b5b861b9ada4e6a08
Michiel Cottaar <MichielCottaar@protonmail.com>
2022-08-26 15:17:03 +0100

    Add visual tests for geometry

---
+++ test/visual_tests/geometry/geometry.jl
@@ -0,0 +1,51 @@
+@testset "Geometry plots" begin
+
+dir = @__DIR__
+isCI = get(ENV, "CI", "false") == "true"
+@testset "Plot walls" begin
+    geometry = [
+        mr.walls(repeats=1),
+        mr.walls(rotation=:y),
+        mr.walls(repeats=1, rotation=[1, 1, 1]),
+    ]
+    pp = mr.PlotPlane()
+
+    function plot_walls(fname)
+        f = Figure()
+        mr.plot_geometry(f[1, 1], pp, geometry)
+        CairoMakie.save(fname, f)
+    end
+
+    @visualtest plot_walls "$dir/walls.png" !isCI
+end
+@testset "Plot annuli" begin
+    geometry = [
+        mr.annuli(0.2, 0.4, repeats=[1, 1], rotation=[0, 1, 1]),
+    ]
+    pp = mr.PlotPlane()
+
+    function plot_annuli(fname)
+        f = Figure()
+        mr.plot_geometry(f[1, 1], pp, geometry)
+        CairoMakie.save(fname, f)
+    end
+
+    @visualtest plot_annuli "$dir/annuli.png" !isCI
+end
+@testset "Plot myelinated annuli" begin
+    geometry = [
+        mr.annuli(0.2, 0.4, repeats=[1, 1], rotation=[0, 1, 1], myelin=true)
+    ]
+    pp = mr.PlotPlane()
+
+    function plot_myelinated_annuli(fname)
+        f = Figure()
+        mr.plot_off_resonance(f[1, 1], pp, geometry)
+        mr.plot_geometry!(f[1, 1], pp, geometry)
+        CairoMakie.save(fname, f)
+    end
+
+    @visualtest plot_myelinated_annuli "$dir/myelinated_annuli.png" !isCI
+end
+
+end
--- test/visual_tests/run_visual_tests.jl
+++ test/visual_tests/run_visual_tests.jl
@@ -3,6 +3,7 @@ using CairoMakie
 using VisualRegressionTests
 using Gtk
 
+include("geometry/geometry.jl")
 include("sequence/sequence.jl")
 include("snapshot/snapshot.jl")
 include("trajectory/trajectory.jl")
