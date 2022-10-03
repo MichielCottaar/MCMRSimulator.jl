4f4781ea4cb3731057806ee3d4669aaa4050f062
Michiel Cottaar <MichielCottaar@protonmail.com>
2022-10-03 13:32:48 +0100

    Add tests for DWI sequence diagram plots

--- test/visual_tests/sequence/sequence.jl
+++ test/visual_tests/sequence/sequence.jl
@@ -3,14 +3,29 @@
 dir = @__DIR__
 isCI = get(ENV, "CI", "false") == "true"
 
-function plot_perfect_dwi(fname)
-    sequence = mr.perfect_dwi(bval=2.)
-    f = Figure()
-    Axis(f[1, 1])
-    plot!(sequence)
-    CairoMakie.save(fname, f)
+@testset "Instantaneous gradients & pulses" begin
+    function plot_perfect_dwi(fname)
+        sequence = mr.perfect_dwi(bval=2.)
+        f = Figure()
+        Axis(f[1, 1])
+        plot!(sequence)
+        CairoMakie.save(fname, f)
+    end
+
+    @visualtest plot_perfect_dwi "$dir/perfect_dwi.png" !isCI
 end
 
-@visualtest plot_perfect_dwi "$dir/perfect_dwi.png" !isCI
+@testset "Finite gradients" begin
+    function plot_finite_dwi(fname, single_gradient=false)
+        sequence = mr.dwi(bval=2.)
+        f = Figure()
+        Axis(f[1, 1])
+        plot!(sequence; single_gradient=single_gradient)
+        CairoMakie.save(fname, f)
+    end
+
+    @visualtest fn->plot_perfect_dwi(fn, true) "$dir/single_grad_dwi.png" !isCI
+    @visualtest fn->plot_perfect_dwi(fn, false) "$dir/multi_grad_dwi.png" !isCI
+end
 
 end
