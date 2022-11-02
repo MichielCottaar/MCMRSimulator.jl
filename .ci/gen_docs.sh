0564e7e4e3cdfe2dbcf5f1f82292ccdc1d874f11
Michiel Cottaar <MichielCottaar@protonmail.com>
2022-11-02 16:30:29 +0000

    Instantiate package before building docs

--- .ci/gen_docs.sh
+++ .ci/gen_docs.sh
@@ -2,6 +2,6 @@
 apt-get update
 apt-get install -y git
 
-julia --project=docs docs/make.jl
+julia --project=docs -e 'using Pkg; Pkg.develop(PackageSpec(path=pwd())); Pkg.instantiate(); include("docs/make.jl")'
 mkdir -p public
 mv docs/build public/$1
