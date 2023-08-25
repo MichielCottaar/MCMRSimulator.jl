70801a845e2b36e54eb2bd0f2e39e828b5120004
Michiel Cottaar <MichielCottaar@protonmail.com>
2023-08-25 18:02:54 +0100

    Add tests for points close to triangle

--- test/test_offresonance.jl
+++ test/test_offresonance.jl
@@ -86,5 +86,16 @@
                 @test field(mesh, [0, 0, -1]) ≈ atan(-sqrt(3)) + atan(1)
             end
         end
+        @testset "Points very close to mesh" begin
+            for t in ([1, 2, 3], [2, 1, 3])
+                mesh = mr.mesh(vertices=[[0, 0, 0], [1, 0, 0], [1, 1, 0]], triangles=[t], myelin=true, susceptibility_iso=1., susceptibility_aniso=0.)
+                for y in (0.2, 0.3)
+                    @test field(mesh, [0.8, y, 1e-5]) ≈ 0.5
+                    @test field(mesh, [0.8, y, 1e-3]) ≈ 0.5
+                    @test field(mesh, [0.8, y, -1e-5]) ≈ -0.5
+                    @test field(mesh, [0.8, y, -1e-3]) ≈ -0.5
+                end
+            end
+        end
     end
 end
