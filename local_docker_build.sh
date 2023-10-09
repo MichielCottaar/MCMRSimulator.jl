6886bcf8aef07fb33dd767262ffbaed8f88b722b
Michiel Cottaar <MichielCottaar@protonmail.com>
2023-10-09 18:12:56 +0100

    Swap arm64 and amd64 tags, so that they are correct

--- local_docker_build.sh
+++ local_docker_build.sh
@@ -10,14 +10,14 @@ if docker_tag_exists $tag ; then
     echo "Image with tag '$tag' already exists on docker hub."
     exit 1
 fi
-if docker_tag_exists $tag-arm64 ; then
+if docker_tag_exists $tag-amd64 ; then
     echo "Checking out tag '$tag' from git"
     git checkout $tag
-    echo "Building $tag-amd64 locally"
+    echo "Building $tag-arm64 locally"
     docker login
-    docker build -t michielc/mcmr:$tag-amd64 .
-    docker push michielc/mcmr:$tag-amd64
-    docker manifest create michielc/mcmr:$tag michielc/mcmr:$tag-arm64 michielc/mcmr:$tag-amd64
+    docker build -t michielc/mcmr:$tag-arm64 .
+    docker push michielc/mcmr:$tag-arm64
+    docker manifest create michielc/mcmr:$tag michielc/mcmr:$tag-arm64 michielc/mcmr:$tag-arm64
     docker manifest push michielc/mcmr:$tag
 else
     echo "ARM64 image for '$tag' has not been uploaded yet. Please wait for the gitlab pipeline to run before running this script."
