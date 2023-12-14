ad94d0f7fb6b651c6bc2ceb9e910401f4a75902c
Michiel Cottaar <MichielCottaar@protonmail.com>
2023-12-14 14:14:56 +0000

    Fix module name and remove imports

--- ext/MakieMCMRSimulator/MakieMCMRSimulator.jl
+++ ext/MakieMCMRSimulator/MakieMCMRSimulator.jl
@@ -1,4 +1,4 @@
-module Plot
+module MakieMCMRSimulator
 using Makie
 
 
@@ -11,12 +11,4 @@ include("snapshots.jl")
 include("trajectory.jl")
 include("movie.jl")
 
-import .PlotPlanes: PlotPlane
-import .Geometries: plot_geometry, plot_geometry!, plot_geometry3d, plot_geometry3d!
-import .Snapshots: plot_snapshot, image_snapshot, dyad_snapshot
-import .Snapshots: plot_snapshot!, image_snapshot!, dyad_snapshot!
-import .Trajectory: plot_trajectory2d, plot_trajectory3d
-import .Trajectory: plot_trajectory2d!, plot_trajectory3d!
-import .OffResonance: plot_off_resonance, plot_off_resonance!
-import .Movie: simulator_movie
 end
