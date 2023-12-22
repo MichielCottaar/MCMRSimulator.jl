daaa6735aca8bc226f00af370d62571f4f0d2344
Michiel Cottaar <MichielCottaar@protonmail.com>
2023-12-22 11:46:10 +0000

    Fix import order

--- ext/MakieMCMRSimulator/MakieMCMRSimulator.jl
+++ ext/MakieMCMRSimulator/MakieMCMRSimulator.jl
@@ -2,8 +2,8 @@ module MakieMCMRSimulator
 
 include("utils.jl")
 include("sequences.jl")
-include("off_resonance.jl")
 include("geometries.jl")
+include("off_resonance.jl")
 #include("snapshots.jl")
 #include("trajectory.jl")
 #include("movie.jl")
