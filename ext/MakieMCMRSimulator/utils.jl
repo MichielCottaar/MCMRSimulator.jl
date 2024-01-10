5f0529b3af07712cf20eb9dbfcb76d334901ce0c
Michiel Cottaar <MichielCottaar@protonmail.com>
2024-01-10 13:41:50 +0000

    Generalise orientation to color function

--- ext/MakieMCMRSimulator/utils.jl
+++ ext/MakieMCMRSimulator/utils.jl
@@ -5,11 +5,14 @@ import MCMRSimulator.Spins: Spin, SpinOrientation, phase, transverse
 
 """
     color(orient::SpinOrientation; saturation=1.)
+    color(spin::Spin; saturation=1., sequence=1)
 
 Returns a color representing the spin orientation in the transverse (x-y) plane.
 Brighter colors have a larger transverse component, so that spins with no transverse component are black.
 The actual color encodes the spin orientation.
 """
-color(orient::Union{Spin{1}, SpinOrientation}; saturation=1.) = Colors.HSV(phase(orient) + 180, saturation, transverse(orient))
+color(orient::SpinOrientation; saturation=1.) = Colors.HSV(phase(orient) + 180, saturation, transverse(orient))
+color(spin::Spin{N}; sequence=1, kwargs...) where {N} = color(spin.orientations[sequence]; kwargs...)
+
 
 end
