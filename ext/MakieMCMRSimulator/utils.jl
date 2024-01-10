29af02cbe324a4c0d7b91cb02d0d032b6bcf7d96
Michiel Cottaar <MichielCottaar@protonmail.com>
2024-01-10 14:03:20 +0000

    Create 3D plots if no PlotPlane is provided

--- ext/MakieMCMRSimulator/utils.jl
+++ ext/MakieMCMRSimulator/utils.jl
@@ -1,6 +1,9 @@
 module Utils
+import Makie
 import Colors
 import MCMRSimulator.Spins: Spin, SpinOrientation, phase, transverse
+import MCMRSimulator: FixedGeometry, Snapshot, ObstructionGroup
+import MCMRSimulator.Plot: PlotPlane
 
 
 """
@@ -14,5 +17,10 @@ The actual color encodes the spin orientation.
 color(orient::SpinOrientation; saturation=1.) = Colors.HSV(phase(orient) + 180, saturation, transverse(orient))
 color(spin::Spin{N}; sequence=1, kwargs...) where {N} = color(spin.orientations[sequence]; kwargs...)
 
+
+const Projectable = Union{FixedGeometry, ObstructionGroup, Vector{<:ObstructionGroup}, Snapshot, Vector{<:Snapshot}}
+
+Makie.args_preferred_axis(::Projectable) = Makie.LScene
+Makie.args_preferred_axis(::Tuple{PlotPlane, <:Projectable}) = Makie.Axis
 
 end
