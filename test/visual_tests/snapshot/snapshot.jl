6eda43657626a9990229a7fdabcb7a789a9e2786
Michiel Cottaar <MichielCottaar@protonmail.com>
2024-01-11 13:36:55 +0000

    Test 3d plots

--- test/visual_tests/snapshot/snapshot.jl
+++ test/visual_tests/snapshot/snapshot.jl
@@ -2,7 +2,7 @@
     isCI = get(ENV, "CI", "false") == "true"
     dir = @__DIR__
     @testset "Spins as dyads" begin
-        function plot_dyads(fname, kind=:dyad)
+        function plot_dyads(fname; kind=:dyad, project=true)
             snapshot = mr.Snapshot(
                 [
                     mr.Spin(position=[0, 0, 0], transverse=0.8, phase=0.)
@@ -11,14 +11,20 @@
                     mr.Spin(position=[0, 1, 3], transverse=0.8, phase=-90.)
                 ]
             )
-            plot_plane = mr.PlotPlane()
             f = Figure()
-            plot(f[1, 1], plot_plane, snapshot; lengthscale=1., kind=kind)
+            if project
+                plot_plane = mr.PlotPlane()
+                plot(f[1, 1], plot_plane, snapshot; kind=kind)
+            else
+                plot(f[1, 1], snapshot; kind=kind)
+            end
             CairoMakie.save(fname, f)
         end
 
         @visualtest plot_dyads "$dir/dyad_snapshot.png" !isCI
-        @visualtest fn -> plot_dyads(fn, :scatter) "$dir/scatter_snapshot.png" !isCI
+        @visualtest fn -> plot_dyads(fn, kind=:scatter) "$dir/scatter_snapshot.png" !isCI
+        @visualtest fn -> plot_dyads(fn, project=false) "$dir/dyad_snapshot3D.png" !isCI
+        @visualtest fn -> plot_dyads(fn, project=false, kind=:scatter) "$dir/scatter_snapshot3D.png" !isCI
     end
     @testset "Spins as image" begin
         function plot_image(fname)
