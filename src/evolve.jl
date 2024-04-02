56571a47b5b3f9b5fc77af759762aa0a5a188cd8
Michiel Cottaar <MichielCottaar@protonmail.com>
2024-04-02 14:05:24 +0100

    Fix type of RT

--- src/evolve.jl
+++ src/evolve.jl
@@ -69,7 +69,7 @@ function readout(spins, simulation::Simulation{N}, new_readout_times=nothing; bo
         current_TR = Int(div(get_time(snapshot), TR(seq), RoundDown))
         time_in_TR = get_time(snapshot) - current_TR * TR(seq)
 
-        rt = isnothing(new_readout_times) ? readout_times(seq) : readout_times
+        rt = isnothing(new_readout_times) ? readout_times(seq) : new_readout_times
         if rt isa Number
             rt = [rt]
         end
