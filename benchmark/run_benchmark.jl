f3cdbd3d966bfc83a50ea29347a8cdfe21915534
Michiel Cottaar <MichielCottaar@protonmail.com>
2024-08-02 14:01:41 +0100

    print result to markdown

--- benchmark/run_benchmark.jl
+++ benchmark/run_benchmark.jl
@@ -1,26 +1,29 @@
 using PkgBenchmark
+using Markdown
 
- mkconfig(; kwargs...) =
-     BenchmarkConfig(
-         env = Dict(
-             "JULIA_NUM_THREADS" => "1",
-             "OMP_NUM_THREADS" => "1",
-         );
-         kwargs...
-     )
+mkconfig(; kwargs...) =
+    BenchmarkConfig(
+        env = Dict(
+            "JULIA_NUM_THREADS" => "1",
+            "OMP_NUM_THREADS" => "1",
+        );
+        kwargs...
+    )
 
- group_target = benchmarkpkg(
-     dirname(@__DIR__),
-     mkconfig(),
-     resultfile = joinpath(@__DIR__, "result-target.json"),
- )
+group_target = benchmarkpkg(
+    dirname(@__DIR__),
+    mkconfig(),
+    resultfile = joinpath(@__DIR__, "result-target.json"),
+)
 
- group_baseline = benchmarkpkg(
-     dirname(@__DIR__),
-     mkconfig(id = "add-benchmark"),
-     resultfile = joinpath(@__DIR__, "result-baseline.json"),
- )
+group_baseline = benchmarkpkg(
+    dirname(@__DIR__),
+    mkconfig(id = "add-benchmark"),
+    resultfile = joinpath(@__DIR__, "result-baseline.json"),
+)
 
- judgement = judge(group_target, group_baseline)
+judgement = judge(group_target, group_baseline)
 
- displayresult(judgement)
+export_markdown(stdout, group_target)
+
+export_markdown(stdout, judgement)
