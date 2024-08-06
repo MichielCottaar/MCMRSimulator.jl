0124b3c0cf6c9dd25e8ed3c9885a0a6fb6f4de35
Michiel Cottaar <MichielCottaar@protonmail.com>
2024-08-06 16:27:08 +0100

    Allow setting of keywords in load_mesh

--- src/geometries/user/load_mesh.jl
+++ src/geometries/user/load_mesh.jl
@@ -8,7 +8,7 @@ Loads a [`Mesh`](@ref) from a PLY file.
 PLY stands for Polygon File Format (http://paulbourke.net/dataformats/ply/).
 PLY IO is handled by PlyIO.jl (https://github.com/JuliaGeometry/PlyIO.jl).
 """
-function ply_from_mesh(ply_file)
+function ply_from_mesh(ply_file; kwargs...)
     ply = PlyIO.load_ply(ply_file)
     vertices = collect(zip(
         Float32.(ply["vertex"]["x"]),
@@ -16,7 +16,7 @@ function ply_from_mesh(ply_file)
         Float32.(ply["vertex"]["z"]),
     ))
     indices = [v .+ 1 for v in ply["face"]["vertex_indices"]]
-    return Mesh(vertices=vertices, triangles=indices)
+    return Mesh(vertices=vertices, triangles=indices; kwargs...)
 end
 
 
@@ -27,21 +27,21 @@ Loads a [`Mesh`](@ref) from a file.
 
 Currently only PLY files are supported (see [`ply_from_mesh`](@ref))
 """
-function load_mesh(io::IO)
+function load_mesh(io::IO; kwargs...)
     mark(io)
     firstline = readline(io)
     reset(io)
     if firstline == "ply"
-        return ply_from_mesh(io)
+        return ply_from_mesh(io; kwargs...)
     else
         throw(ErrorException("Could not recognise input mesh format!"))
     end
 end
 
 
-function load_mesh(file_name::AbstractString)
+function load_mesh(file_name::AbstractString; kwargs...)
     open(file_name, "r") do fid
-        load_mesh(fid)
+        load_mesh(fid; kwargs...)
     end
 end
 
