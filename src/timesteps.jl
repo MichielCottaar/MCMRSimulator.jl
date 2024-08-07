22bc5d1917c225d576bb68c0bd1af133f1fa9360
Michiel Cottaar <MichielCottaar@protonmail.com>
2024-08-07 18:13:59 +0100

    Use logging

--- src/timesteps.jl
+++ src/timesteps.jl
@@ -31,21 +31,21 @@ function TimeStep(; diffusivity, geometry, timestep=Inf, size_scale=nothing, tur
         )
     idx = argmin(options)
     if verbose
-        println("# Timestep determination")
+        @info "# Timestep determination"
         if idx == 1
-            println("Maximum timestep set by user to $(options[1]) ms.")
+            @info "Maximum timestep set by user to $(options[1]) ms."
         elseif idx == 2
-            println("Maximum timestep set by turtoisity constraint based on size of geometry to $(options[2]) ms.")
+            @info "Maximum timestep set by turtoisity constraint based on size of geometry to $(options[2]) ms."
             if isnothing(size_scale)
-                println("Size scale of smallest object in the simulation was automatically determined to be $(use_size_scale) um.")
-                println("If this value is too small, you can set the size scale explicitly by passing on `size_scale=<new_value>` to the `Simulator` constructor.")
+                @info "Size scale of smallest object in the simulation was automatically determined to be $(use_size_scale) um."
+                @info "If this value is too small, you can set the size scale explicitly by passing on `size_scale=<new_value>` to the `Simulator` constructor."
             else
-                println("Size scale was set to $(use_size_scale) um by the user.")
+                @info "Size scale was set to $(use_size_scale) um by the user."
             end
         elseif idx == 3
-            println("Maximum timestep set by requirement to get sufficient transitions from free to bound spins to $(options[3]) ms.")
+            @info "Maximum timestep set by requirement to get sufficient transitions from free to bound spins to $(options[3]) ms."
         end
-        println("The actual timestep might be further reduced based on the MR sequence(s).")
+        @info "The actual timestep might be further reduced based on the MR sequence(s)."
     end
     return TimeStep(minimum(options), (isnothing(gradient_precision) ? (1e-4 * precision) : gradient_precision) / diffusivity)
 end
