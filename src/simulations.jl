3485c6d97c0ac9317b6afd315a6f54a2186d5e08
Michiel Cottaar <MichielCottaar@protonmail.com>
2024-10-03 16:41:36 +0100

    Randomise where field is evaluated between start and end of trajectory

--- src/simulations.jl
+++ src/simulations.jl
@@ -198,7 +198,10 @@ The field is computed in ppm. Knowledge of the scanner [`B0`](@ref) is needed to
 """
 susceptibility_off_resonance(simulation::Simulation, spin::Spin) = susceptibility_off_resonance(simulation, spin.position, stuck(spin) ? spin.reflection.inside : nothing)
 susceptibility_off_resonance(simulation::Simulation, position::AbstractVector, inside::Union{Nothing, Bool}=nothing) = susceptibility_off_resonance(simulation.susceptibility, SVector{3, Float64}(position), inside)
-susceptibility_off_resonance(simulation::Simulation, old_pos::AbstractVector, new_pos::AbstractVector) = susceptibility_off_resonance(simulation.susceptibility, (old_pos .+ new_pos) ./2, nothing)
+function susceptibility_off_resonance(simulation::Simulation, old_pos::AbstractVector, new_pos::AbstractVector) 
+    along_tract = rand()
+    susceptibility_off_resonance(simulation.susceptibility, along_tract * old_pos .+ (1 - along_tract) .* new_pos, nothing)
+end
 
 
 end
