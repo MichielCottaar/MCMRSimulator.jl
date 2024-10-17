bd1c28ae29aba98436aad20f50873af85bf1318b
Michiel Cottaar <MichielCottaar@protonmail.com>
2024-10-17 17:32:19 +0100

    Increase initial timestep for permeability based on simulations

--- src/timesteps.jl
+++ src/timesteps.jl
@@ -3,15 +3,15 @@ import ..Constants: gyromagnetic_ratio
 import ..Geometries: Internal
 
 """
-    TimeStep(simulation; timestep=Inf, turtoisity_precision=3e-2 * <precision>, gradient_precision=1e-4 * <precision>, precision=1., max_permeability_probability=0.01)
+    TimeStep(simulation; timestep=Inf, turtoisity=3e-2, gradient=1e-4, permeability=0.5,)
 
 Creates an object controlling the timestep of the MCMR simulation.
 
 At any time the timestep is guaranteed to be shorter than:
 1. user-provided value of `timestep` (in ms).
 2. `FullTimeStep.turtoisity_precision` * `size_scale(geometry)`^2 / D, where [`size_scale`](@ref) is the average size of the obstructions and `D` is the [`diffusivity`](@ref).
-3. timestep that would allow permeability probability greater than `max_permeability_probability` (default: 1%).
-4. teimstep that would allow magnetisation transfer probability to be greater than 25%.
+3. timestep greater than `permeability` times 1 / (maximum permeability parameter)^2
+4. timestep that would allow magnetisation transfer probability to be greater than 25%.
 4. the minimum dwell time of the bound pool.
 5. (`FullTimeStep.gradient_precision` /( D * \\gamma^2 * G^2))^(1//3), where \\gamma is the [`gyromagnetic_ratio`](@ref) and `G` is the current `gradient_strength`.
 """
@@ -22,17 +22,17 @@ end
 
 
 function TimeStep(; 
-    diffusivity, geometry, timestep=Inf, size_scale=nothing, 
-    turtoisity_precision=nothing, gradient_precision=nothing, precision=1., verbose=true,
-    max_permeability_probability=0.05,
+    diffusivity, geometry, size_scale=nothing, 
+    turtoisity=3e-2, gradient=nothing, verbose=true,
+    max_permeability_probability=0.5,
     )
     if iszero(diffusivity)
-        return TimeStep(timestep, Inf)
+        return TimeStep(Inf, Inf)
     end
     use_size_scale = isnothing(size_scale) ? Internal.size_scale(geometry) : size_scale
     options = (
             timestep,
-            (isnothing(turtoisity_precision) ? (3e-2 * precision) : turtoisity_precision) * use_size_scale^2 / diffusivity,
+            turtoisity * use_size_scale^2 / diffusivity,
             Internal.max_timestep_sticking(geometry, diffusivity),
             max_timestep_permeability(geometry, max_permeability_probability),
         )
@@ -57,7 +57,7 @@ function TimeStep(;
         push!(lines, "The actual timestep might be further reduced based on the MR sequence(s).")
         @info join(lines, '\n')
     end
-    return TimeStep(minimum(options), (isnothing(gradient_precision) ? (1e-4 * precision) : gradient_precision) / diffusivity)
+    return TimeStep(minimum(options), gradient / diffusivity)
 end
 
 (ts::TimeStep)(gradient_strength) = min(
@@ -66,11 +66,11 @@ end
 )
 
 """
-    max_timestep_permeability(geometry, max_permeability_probability)
+    max_timestep_permeability(geometry, scaling)
 
-Compute the maximum timestep necessary to keep the probability of a spin passing through any not fully permeable surface below 25%.
+Compute the maximum timestep necessary to keep the probability of a spin passing through sufficiently low.
 """
-max_timestep_permeability(geometry, max_permeability_probability) = (max_permeability_probability / Internal.max_permeability_non_inf(geometry))^2
+max_timestep_permeability(geometry, scaling) = scaling * (1. / Internal.max_permeability_non_inf(geometry))^2
 
 
 end
