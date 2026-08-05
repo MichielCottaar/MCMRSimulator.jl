1d54658389145e785bc41e3bcd71976ad5a5dc8c
Michiel Cottaar <MichielCottaar@protonmail.com>
2026-08-05 16:51:56 +0100

    Fixed initial magnetisation value

--- test/test_evolve.jl
+++ test/test_evolve.jl
@@ -28,7 +28,7 @@
     @testset "Gradient echo sequence" begin
         simulation = mr.Simulation(mr.read_pulseq(joinpath(@__DIR__, "pulseq", "gradient_echo_TE_2.8.seq")))
         snaps = mr.readout(zeros(3), simulation, 0:0.5:2.8)
-        @test mr.orientation(snaps[1]) ≈ SA[1., 0., 0.]
+        @test mr.orientation(snaps[1]) ≈ SA[0., 0., 1.]
         for snap in snaps[2:end]
             @test mr.orientation(snap) ≈ SA[0., -1., 0.]
         end
