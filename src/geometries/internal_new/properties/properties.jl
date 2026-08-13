fa32954279c40d801e0f455f1359ffd5d8991007
Michiel Cottaar <MichielCottaar@protonmail.com>
2026-08-13 15:11:57 +0100

    Unify `Vector/Tuple` interface

--- src/geometries/internal_new/properties/properties.jl
+++ src/geometries/internal_new/properties/properties.jl
@@ -16,7 +16,7 @@ function get_value(property, ::ObstructionIndex)
     property
 end
 
-function get_value(properties::GeometryVectorProperties, index::ObstructionIndex)
+function get_value(properties::Union{GeometryVectorProperties, GeometryTupleProperties}, index::ObstructionIndex)
     child_index = index.indices[1]
     1 <= child_index <= length(properties.properties) ||
         throw(BoundsError(properties.properties, child_index))
@@ -27,16 +27,6 @@ function get_value(properties::GeometryVectorProperties, index::ObstructionIndex
     get_value(properties.properties[child_index], remaining)
 end
 
-function get_value(properties::GeometryTupleProperties, index::ObstructionIndex)
-    child_index = index.indices[1]
-    1 <= child_index <= length(properties.properties) ||
-        throw(BoundsError(properties.properties, child_index))
-    remaining_length = length(index.indices) - 1
-    remaining = ObstructionIndex{remaining_length}(
-        SVector{remaining_length, Int}(index.indices[2:end]),
-    )
-    get_value(properties.properties[child_index], remaining)
-end
 
 get_value(property, indices::Vector{<:ObstructionIndex}) = [
     get_value(property, index) for index in indices
