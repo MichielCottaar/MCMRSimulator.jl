059c558ff5e8f18ee54be93d2ca72c32feb476c7
Michiel Cottaar <MichielCottaar@protonmail.com>
2026-08-17 15:02:59 +0100

    Remove helper function

--- src/geometries/internal/physical_geometries/base_obstructions/base_obstructions.jl
+++ src/geometries/internal/physical_geometries/base_obstructions/base_obstructions.jl
@@ -19,23 +19,18 @@ include("rounds.jl")
 include("overlapping_rounds.jl")
 include("triangles.jl")
 
-function surface_samples_to_intersection(
-    positions::AbstractVector{SVector{N, Float64}},
-    normals::AbstractVector{SVector{N, Float64}},
+
+function random_surface_positions(
+    geometry::BaseObstruction{N}, density::GeometryLeafProperties,
+    bounding_box::InternalBoundingBoxes.InternalBoundingBox{N}, scale_density,
 ) where {N}
+    positions, normals = surface_sampling(geometry, density, bounding_box, scale_density)
     intersections = Intersection{N}[]
     for normal in normals
         intersection = Intersection(0.0, normal, true, ObstructionIndex(), false)
         push!(intersections, rand(Bool) ? intersection : flip(intersection))
     end
     positions, intersections
-end
-
-function random_surface_positions(
-    geometry::BaseObstruction{N}, density::GeometryLeafProperties,
-    bounding_box::InternalBoundingBoxes.InternalBoundingBox{N}, scale_density,
-) where {N}
-    surface_samples_to_intersection(surface_sampling(geometry, density, bounding_box, scale_density)...)
 end
 
 end
